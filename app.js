f7b010d8a031427b4bbb62c669bc283bc9bbc4a3
rlakshvibes <ramyafilter@gmail.com>
2025-08-02 22:38:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Firebase Configuration
 // TODO: Replace with your Firebase config
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbrboEJx7hOFSRO0l7QbJAJkjejoEUpb4",
+  authDomain: "daily-tracker-f025c.firebaseapp.com",
+  projectId: "daily-tracker-f025c",
+  storageBucket: "daily-tracker-f025c.firebasestorage.app",
+  messagingSenderId: "790448833460",
+  appId: "1:790448833460:web:a327ebdf2d77d6f22228e1",
+  measurementId: "G-1TDE6QWYB4"
+};
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
